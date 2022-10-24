a4b113aac01c8127804db9050d7568a55e0d1714
feisal <adanfeisal18@gmaile.com>
2022-10-24 21:55:40 +0300

    Updated

--- arrays-callbacks.js
+++ arrays-callbacks.js
@@ -20,7 +20,17 @@ const zooAnimals = [
   The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
   
   */
+  
   const displayNames = [];
+  const zoo = [
+   {animal_name:"Jackel",scientific_name:"Canine"}
+  ]
+  function animalNames({animal_name, scientific_name}){
+    return `name: ${animal_name}, scientific: ${scientific_name}`
+  }
+  zooAnimals.forEach((element)=>{
+   displayNames.push(animalNames(element));
+  })
   console.log(displayNames);
   
   /* Request 2: .map()
@@ -30,25 +40,33 @@ const zooAnimals = [
   */
   
   const lowCaseAnimalNames = [];
-  console.log(lowCaseAnimalNames);
+ const animalsnames = zooAnimals.map(item => item.animal_name.toLowerCase())
+  console.log(animalsnames);
   
   /* Request 3: .filter() 
   
   The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
   
   */
-  const lowPopulationAnimals = [];
-  console.log(lowPopulationAnimals);
+  // const lowPopulationAnimals = []
+
+const lowPopulationAnimals = zooAnimals.filter(item => item.population < 5 );
+console.log(lowPopulationAnimals);
   
   /* Request 4: .reduce() 
   
   The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
   
   */
-  let populationTotal = 0;
-  console.log(populationTotal);
+  // let populationTotal = 0;
   
   
+  const populationTotal = zooAnimals.reduce(function(accumulator, currentvalue){
+  console.log(`total animal population ${accumulator}`)
+  console.log(`adding animal population ${currentvalue.population}`)
+return accumulator + currentvalue.population;
+},  0 );
+console.log(populationTotal);
   // ==== Callbacks ====  
   
   /* Step 1: Create a higher-order function
@@ -57,6 +75,15 @@ const zooAnimals = [
     * The last parameter accepts a callback
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
+  function contains(a, b, cb) {
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
+    if (b.includes(a) === true) {
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
   
   
   /* Step 2: Create several functions to callback with consume();
@@ -64,13 +91,42 @@ const zooAnimals = [
     * Create a function named multiply that returns the product of two numbers 
     * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
   */
-  
-  
+  // Function Add
+ function add(num1, num2) {
+  return num1 + num2;
+ } 
+ console.log(add(6, 5));
+// Function Multiply
+function multiply(num1, num2) {
+  return num1 * num2;
+ } 
+ console.log(multiply(6, 5));
+//  function greeting
+function greeting(first_name, last_name) {
+  return `"Hello ${first_name}  ${last_name}, nice to meet you!"`;
+}
+console.log(greeting("Feisal", "Adan"));
+
   /* Step 3: Check your work by un-commenting the following calls to consume(): */
-  // console.log(consume(2, 2, add)); // 4
-  // console.log(consume(10, 16, multiply)); // 160
-  // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-  
+  console.log(consume(2, 2, add)); // 4
+  console.log(consume(10, 16, multiply)); // 160
+  console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+//     // Function Add
+//  function add(num1, num2) {
+//   return num1 + num2;
+//  } 
+//  console.log(add(2, 2));
+// // Function Multiply
+// function multiply(num1, num2) {
+//   return num1 * num2;
+//  } 
+//  console.log(multiply(10, 16));
+// //  function greeting
+// function greeting(first_name, last_name) {
+//   return `"Hello ${first_name}  ${last_name}, nice to meet you!"`;
+// }
+// console.log(greeting("Mary", "Poppins"));
+
   
   
   
